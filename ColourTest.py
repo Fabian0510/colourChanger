bc387e827defb9df4f99a3fc1761ae2c8cc86e0c
Geoff Jones <geoffyj86@gmail.com>
2018-07-03 13:06:34 +0100

    Initial file creation

---
+++ ColourTest.py
@@ -0,0 +1,9 @@
+from Adafruit_IO import *
+aio = Client('583f4cd618a54046871ba8dc4c0e06f9')
+def ChangeColour(inputColour):
+    choices = {'blue':(0,0,253),'red':(255,0,0),'green':(0,255,0),'orange':(254,138,0),'yellow':(254,254,0)}
+    result = choices.get(inputColour)
+    print(result)
+
+while True:
+    ChangeColour(input('enter colour'))
